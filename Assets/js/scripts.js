259668f65fa3106e938706dd7e769eba44b7102b
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 16:54:24 -0500

    Adds more articles to page

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -5,3 +5,9 @@ var contentParagraph = document.querySelector(".content");
 arrowClicked.addEventListener("click", function () {
   contentParagraph.className="content-clicked";
 });
+
+var arrowHover = document.querySelector(".arrow");
+
+arrowHover.addEventListener("hover", function () {
+  arrowHover
+});
