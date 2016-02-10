df2791cffa2b4f89316b925ff3ddf4963745e85f
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 15:12:48 -0500

    Adds some accordion functionality to one element

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,25 +1,7 @@
 
-var arrowChange = document.querySelector(".arrow");
+var arrowClicked = document.querySelector(".arrow");
+var contentParagraph = document.querySelector(".content");
 
-arrowChange.addEventListener("mouseover", function () {
-  arrowChange.style.color="red";
-  }
-);
-
-// arrowChange.addEventListener("click", function () {
-//   alert("you clicked the arrow!");
-//   }
-// );
-
-// var paragraphContent = document.querySelector(".content");
-//
-// paragraphContent.addEventListener("ACTION HERE", function () {
-//   //what do you want the paragraphContent to do?
-//   }
-// );
-
-var paragraphContent = document.querySelector(".content");
-
-arrowChange.addEventListener("click", function () {
-  paragraphContent.className.replace = "content-clicked";
+arrowClicked.addEventListener("click", function () {
+  contentParagraph.className="content-clicked";
 });
