a72bccd59559a6df94b9a4aabb3a3c6674a3447c
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 17:35:03 -0500

    Allows accordion to both open and collapse

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,12 +2,24 @@
 var arrowClicked = document.querySelector(".arrow");
 var contentParagraph = document.querySelector(".content");
 
-arrowClicked.addEventListener("click", function () {
-  contentParagraph.className="content-clicked";
-});
+//this was working i just commented it out
+// arrowClicked.addEventListener("click", function () {
+//   contentParagraph.className="content-clicked";
+// });
 
 var arrowHover = document.querySelector(".arrow");
 
-arrowHover.addEventListener("hover", function () {
-  arrowHover
+// arrowHover.addEventListener("hover", function () {
+//   arrowHover
+// });
+
+// var classList = document.GETALLTHECLASSNAMES
+//
+// document.getElementsByClassName("content").classList.toggle("content-clicked");
+
+arrowClicked.addEventListener("click", function (){
+  if (contentParagraph.className ===("content")){
+    contentParagraph.className="content-clicked";
+  } else if (contentParagraph.className===("content-clicked")){
+    contentParagraph.className="content"}
 });
