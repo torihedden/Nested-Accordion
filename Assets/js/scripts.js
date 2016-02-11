5f539819ba78c8e21506aed02167e035c0d7f58f
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 21:25:10 -0500

    Manually adds accordion funcitonality to second content piece

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,17 +1,5 @@
-//this was working i just commented it out
-// arrowClicked.addEventListener("click", function () {
-//   contentParagraph.className="content-clicked";
-// });
 
-// var arrowHover = document.querySelector(".arrow");
 
-// arrowHover.addEventListener("hover", function () {
-//   arrowHover
-// });
-
-// var classList = document.GETALLTHECLASSNAMES
-//
-// document.getElementsByClassName("content").classList.toggle("content-clicked");
 
 var arrowClicked = document.querySelector(".arrow");
 var contentParagraph = document.querySelector(".content");
@@ -22,3 +10,30 @@ arrowClicked.addEventListener("click", function (){
   } else if (contentParagraph.className===("content-clicked")){
     contentParagraph.className="content"}
 });
+
+//change icon color and orientation when you run mouse over the arrow.
+//mouseenter
+//mouseleave
+
+var arrowClicked2 = document.querySelector(".arrow2");
+var contentParagraph2 = document.querySelector(".content2");
+
+arrowClicked2.addEventListener("click", function (){
+  if (contentParagraph2.className ===("content2")){
+    contentParagraph2.className="content-clicked2";
+  } else if (contentParagraph2.className===("content-clicked2")){
+    contentParagraph2.className="content2"}
+});
+
+
+// var arrowList = document.getElementsByClassName(".arrow");
+// var contentList = document.getElementsByClassName(".content");
+//
+// for (i = 0; i < arrowList.length; i++){
+//   arrowList.addEventListener("click", function (){
+//     if (contentList[i].className ===("content")){
+//       contentList[i].className = "content-clicked";
+//     } else if (contentList[i].className ===("content-clicked")) {
+//       contentList[i].className="content"}
+//   })
+// };
