b9a3c9b63632ab2af5dc0b5bb298a86a50d2f2d7
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 17:31:14 -0500

    Adds sub-accordions, all open at same time

--- assets/js/jquery-version.js
+++ assets/js/jquery-version.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
   'use strict'
 
   $(".header").click(".header", function(){
-    // $(".content").toggleClass("content-clicked");
+
     $($(this).next()).toggle().addClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
     //now i just removed that .removeClass and it still works. weird.
     //.toggle() by default will toggle display attributes of the targeted element.
     //but I need to keep toggling that CSS class to maintain the styling I applied to the text
     $($(this).find("i")).toggleClass("arrow-down");
     //now only the arrow of the header you clicked on will rotate and change color. it reverses, too, on the next click.
+  });
+
+  $(".sub-header").click(".sub-header", function(){
+    $(".sub-para").toggle().addClass("sub-showing");
+
   });
 
 });
