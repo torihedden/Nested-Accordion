2f44ab4f1a0a8531da4b516f2605a973c50ae358
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 15:46:09 -0500

    Adds only one accordion opening at a time

--- assets/js/jquery-version.js
+++ assets/js/jquery-version.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 
   $(".header").click(".header", function(){
     // $(".content").toggleClass("content-clicked");
-    $(".content").toggle().addClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
+    $($(this).next()).toggle().addClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
     //now i just removed that .removeClass and it still works. weird.
     //.toggle() by default will toggle display attributes of the targeted element.
     //but I need to keep toggling that CSS class to maintain the styling I applied to the text
-    console.log(this.children);
     $("i").toggleClass("arrow-down");
+    console.log($(this).next());
   });
 
 });
