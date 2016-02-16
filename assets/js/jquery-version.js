34b0d86a51fd14e5a35b632024700121d45d05e4
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 13:59:15 -0500

    All accordions open and close together

--- assets/js/jquery-version.js
+++ assets/js/jquery-version.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
 
   $(".header").click(".content", function(){
     // $(".content").toggleClass("content-clicked");
-    $(".content").toggle().addClass("content-clicked").removeClass("contnet");
-
+    $(".content").toggle().addClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
+    //now i just removed that .removeClass and it still works. weird.
   });
+
+  
 
 });
