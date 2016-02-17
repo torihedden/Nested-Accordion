2d41665aeed411bdf98ce5be07c32273651991f5
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 20:57:11 -0500

    Fixes toggleClass to actually toggle class

--- assets/js/jquery-version.js
+++ assets/js/jquery-version.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
 
   $(".header").click(".header", function(){
 
-    $($(this).next()).toggle().addClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
+    $($(this).next()).toggleClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
     //now i just removed that .removeClass and it still works. weird.
     //.toggle() by default will toggle display attributes of the targeted element.
     //but I need to keep toggling that CSS class to maintain the styling I applied to the text
     $($(this).find("i")).toggleClass("arrow-down");
     //now only the arrow of the header you clicked on will rotate and change color. it reverses, too, on the next click.
+    //when you click on this header, do any other "content" have class "content-clicked"?
+    //IF so, remove that class, or set display to none
+
     });
 
-    $(".sub-header").click(".sub-header", function(){
+  $(".sub-header").click(".sub-header", function(){
     $($(this).find(".sub-para")).toggle().addClass("sub-showing");
 
     $($(this).find("i")).toggleClass("arrow-down");
