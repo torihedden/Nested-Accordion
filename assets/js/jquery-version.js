30f2602e3d1be07ca638cfbd05e5696f2ca92362
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 22:01:45 -0500

    Each accordion closes when a new one is opened

--- assets/js/jquery-version.js
+++ assets/js/jquery-version.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
   'use strict'
 
-  $(".header").click(".header", function(){
+  $(".header").click(".header", function(){ //
+    var lastClicked = $(".content-clicked");//jQuery returns an array based on the selector you give it.
+                                            //even if that is an array with one element in it. even an empty array.
+                                            //if you expect this to return a single element, you will be disappointed.
+                                            //this is why we target the element at index 0. It will be the first (and only) element in the array, in this case.
+    $(".content-clicked").removeClass("content-clicked");
 
-    $($(this).next()).toggleClass("content-clicked");//.removeClass("contnet");//this class name is a typo but the accordion works. investigate later.
-    //now i just removed that .removeClass and it still works. weird.
-    //.toggle() by default will toggle display attributes of the targeted element.
-    //but I need to keep toggling that CSS class to maintain the styling I applied to the text
+    if (lastClicked[0] !== $(this).next()[0]){
+      $($(this).next()).toggleClass("content-clicked");
+    }
+                                            //.toggle() by default will toggle display attributes of
+                                            //the targeted element
+                                            //but I need to keep toggling that CSS class to maintain the styling I applied to the text.
+                                            //use .toggleClass instead
+                                            
     $($(this).find("i")).toggleClass("arrow-down");
-    //now only the arrow of the header you clicked on will rotate and change color. it reverses, too, on the next click.
-    //when you click on this header, do any other "content" have class "content-clicked"?
-    //IF so, remove that class, or set display to none
-
+                                            //now only the arrow of the header you clicked on will rotate and change color. it reverses, too, on the next click.
+                                            //when you click on this header, do any other "content" have class "content-clicked"?
+                                            //IF so, remove that class, or set display to none
     });
 
   $(".sub-header").click(".sub-header", function(){
-    $($(this).find(".sub-para")).toggle().addClass("sub-showing");
+    $(".sub-showing").removeClass("sub-showing");
+    $($(this).find(".sub-para")).addClass("sub-showing");
 
     $($(this).find("i")).toggleClass("arrow-down");
 
